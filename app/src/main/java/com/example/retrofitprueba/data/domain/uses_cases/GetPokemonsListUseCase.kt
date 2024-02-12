8f0ec112f35f866e955cd4fe584265475215273d
fedeee1 <federico.martinez@vasscompany.com>
2024-02-12 19:30:10 +0100

    DetailsFragment

--- app/src/main/java/com/example/retrofitprueba/data/domain/uses_cases/GetPokemonsListUseCase.kt
+++ app/src/main/java/com/example/retrofitprueba/data/domain/uses_cases/GetPokemonsListUseCase.kt
@@ -4,11 +4,12 @@ import com.example.retrofitprueba.commons.GET_LIST_POKEMON_LIMIT
 import com.example.retrofitprueba.commons.GET_LIST_POKEMON_OFFSET
 import com.example.retrofitprueba.data.PokemonsRepository
 import com.example.retrofitprueba.data.domain.model.pokemon.PokemonModel
-import com.example.retrofitprueba.data.domain.repository.remote.mapper.GetListPokemonResultMapper
+import com.example.retrofitprueba.data.domain.repository.remote.mapper.pokemon.GetListPokemonResultMapper
 import javax.inject.Inject
 
 class GetPokemonsUseCase @Inject constructor(private val repository: PokemonsRepository,
-                                             private val getListPokemonResultMapper: GetListPokemonResultMapper){
+                                             private val getListPokemonResultMapper: GetListPokemonResultMapper
+){
     suspend operator fun invoke() : List<PokemonModel> = getListPokemonResultMapper.fromResponse(repository.getPokemons(
         GET_LIST_POKEMON_LIMIT, GET_LIST_POKEMON_OFFSET)).results
 }
